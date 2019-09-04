75b4cbfe108680dd0c98e0ea8b0737cd5a931a25
jupabass89 <jupabass89@gmail.com>
2019-09-04 12:21:51 -0500

    some refactoring were added due to some improvements recomended in the last PR review

--- app/models/usuario.js
+++ app/models/usuario.js
@@ -4,7 +4,7 @@ module.exports = (sequelize, DataTypes) => {
     'User',
     {
       name: { type: DataTypes.STRING, allowNull: false },
-      last_name: { type: DataTypes.STRING, allowNull: false },
+      lastName: { type: DataTypes.STRING, allowNull: false, field: 'last_name' },
       email: { type: DataTypes.STRING, unique: true, allowNull: false },
       password: { type: DataTypes.STRING, allowNull: false }
     },
