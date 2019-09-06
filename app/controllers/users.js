f3bb357e0d7507d9743aaccadf45920a2a63d9f9
jupabass89 <jupabass89@gmail.com>
2019-09-05 19:36:58 -0500

    refactoring and improving code due to some advices in the last PR

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,16 +1,23 @@
 const logger = require('../logger');
 const userService = require('../services/users');
 const { registerBodyMapper } = require('../mappers/users');
+const errors = require('../errors');
 
 exports.registerUser = (req, res, next) => {
   logger.info('Starting the user creation');
   const user = registerBodyMapper(req.body);
   return userService
-    .registerUser(user, req)
-    .then(response => {
-      logger.info(response.name);
-      logger.info('Success user creation');
-      return res.send(response);
+    .validateUser(user)
+    .then(responseUser => {
+      if (!responseUser) return Promise.reject(errors.databaseError('user already exists'));
+      return userService
+        .registerUser(user, req)
+        .then(response => {
+          logger.info(response.name);
+          logger.info('Success user creation');
+          return res.send(response);
+        })
+        .catch(next);
     })
     .catch(next);
 };
--- app/services/users.js
+++ app/services/users.js
@@ -5,5 +5,13 @@ const { User } = require('../models');
 exports.registerUser = user =>
   User.create(user).catch(error => {
     logger.error(error);
-    return Promise.reject(errors.databaseError('database error'));
+    return Promise.reject(errors.databaseError(error.message));
   });
+
+exports.validateUser = user => {
+  const find = { where: { email: user.email, name: user.name } };
+  return User.findOne(find).catch(error => {
+    logger.error(error);
+    return Promise.reject(errors.databaseError(error.message));
+  });
+};
