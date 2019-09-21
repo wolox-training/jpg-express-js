7f7eb87efb516b33957a7690d851388a72ec5bd8
jupabass89 <jupabass89@gmail.com>
2019-09-20 21:30:56 -0500

    improving data shown

--- app/serializers/users.js
+++ app/serializers/users.js
@@ -1,10 +1,10 @@
-exports.serializeUsersResponse = (users, req) => {
-  const us = users.rows.map(user => ({
+exports.serializeUsersResponse = (allUsers, req) => {
+  const users = allUsers.rows.map(user => ({
     name: user.name,
     lastName: user.lastName,
     email: user.email
   }));
-  const totalPages = Math.ceil(users.count / req.query.limit);
+  const totalPages = Math.ceil(allUsers.count / req.query.limit);
   const currentPage = req.query.page;
-  return { us, total_pages: totalPages, current_page: currentPage };
+  return { users, total_pages: totalPages, current_page: currentPage };
 };
