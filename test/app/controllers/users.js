11f9861c36a672a5c842c0ebd4881c4ac92716f0
jupabass89 <jupabass89@gmail.com>
2019-09-23 23:38:00 -0500

    doing some tests

--- test/app/controllers/users.js
+++ test/app/controllers/users.js
@@ -3,7 +3,6 @@ const request = require('supertest');
 const app = require('../../../app');
 const { mockUser } = require('../../mocks/users');
 const { factoryByModel } = require('../../factory/factory_by_models');
-const token = require('../../../app/helpers/token');
 
 const user = mockUser();
 
@@ -78,31 +77,58 @@ describe('POST /users/sessions', () => {
     }));
 });
 
-describe('GET /users', () => {
-  it.only('should return a list of users', () => {
-    factory.create('User').then(async res => {
-      const us = res.dataValues;
-      const mockU = {
-        name: us.name,
-        lastName: us.lastName,
-        email: us.email,
-        password: 12345678
-      };
-      const tok = await token.createToken(mockU);
-      return request(app)
-        .get('/users')
-        .send({ 'x-access-token': tok })
-        .expect(200);
+describe.only('GET /users', () => {
+  const password = '$2b$10$q0/nJGRvSyZz3i7fgvTY2OwMl4MPozMQI/62Bkz5F88tSl.3Y2W4u';
+  const mockEmail = 'juan@wolox.co';
+  const mockPass = '12345678';
+  beforeEach(() => {
+    factory.create('User', {
+      email: mockEmail,
+      password
     });
   });
 
-  it.only('should fail due to bad authorization token', () => {
-    factory.create('User').then(() => {
-      const tok = 'dfghjkl';
-      return request(app)
-        .get('/users')
-        .send({ 'x-access-token': tok })
-        .expect(400);
-    });
-  });
+  it('should return a single user query successfully', () =>
+    request(app)
+      .post('/users/sessions')
+      .send({ email: mockEmail, password: mockPass })
+      .then(response =>
+        request(app)
+          .get('/users')
+          .set({ 'x-access-token': response.body.token })
+      )
+      .then(response => {
+        expect(200);
+        expect(response.body).toHaveProperty('users');
+      }));
+
+  it('should return a 10 user query successfully', () =>
+    factory.createMany('User', 10).then(() =>
+      request(app)
+        .post('/users/sessions')
+        .send({ email: mockEmail, password: mockPass })
+        .then(response =>
+          request(app)
+            .get('/users')
+            .set({ 'x-access-token': response.body.token })
+        )
+        .then(response => {
+          expect(200);
+          expect(response.body).toHaveProperty('users');
+          expect(response.body.users.length).toBe(10);
+        })
+    ));
+
+  it('should fail for a no authorizated user', () =>
+    request(app)
+      .post('/users/sessions')
+      .send({ email: mockEmail, password: mockPass })
+      .then(() =>
+        request(app)
+          .get('/users')
+          .set({ 'x-access-token': 'bad-token' })
+      )
+      .then(response => {
+        expect(response.statusCode).toBe(400);
+      }));
 });
