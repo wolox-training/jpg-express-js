765b3c9ce6e6e3df6a3c08f33f75d3fb36518b16
jupabass89 <jupabass89@gmail.com>
2019-09-25 16:08:40 -0500

    refactoring properties

--- config/index.js
+++ config/index.js
@@ -1,7 +1,10 @@
 const ENVIRONMENT = process.env.NODE_ENV || 'development';
-const dotenv = require('dotenv');
 
-if (ENVIRONMENT !== 'production') dotenv.config();
+if (ENVIRONMENT !== 'production') {
+  // eslint-disable-next-line global-require
+  const dotenv = require('dotenv');
+  dotenv.config();
+}
 
 const configFile = `./${ENVIRONMENT}`;
 
