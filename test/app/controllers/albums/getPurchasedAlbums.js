24ddfe79229e79090cedeb2c1d38ef6aeab88dcf
jupabass89 <jupabass89@gmail.com>
2019-10-02 01:38:55 -0500

    refactoring test due to some bugs

--- test/app/controllers/albums/getPurchasedAlbums.js
+++ test/app/controllers/albums/getPurchasedAlbums.js
@@ -1,41 +1,74 @@
 const request = require('supertest');
+const nock = require('nock');
 const app = require('../../../../app');
 const helper = require('../../testHelper');
+const { User } = require('../../../../app/models');
+const token = require('../../../../app/helpers/token');
+
+const agent = request(app);
+const admin = true;
+const mockResponse = {
+  userId: 2,
+  id: 2,
+  title: 'quidem molestiae enim'
+};
 
 describe('GET /users/:id/albums', () => {
-  const agent = request(app);
-  const admin = true;
-  test('Should be a succesfull album query of a default user', () =>
-    helper.createUserAndLogin(!admin).then(logIn => {
-      agent
-        .post('/albums/2/')
-        .set('x-access-token', logIn.body.token)
-        .then(() => {
-          agent
-            .get('/users/1/albums')
-            .set('x-access-token', logIn.body.token)
-            .then(response => expect(response.statusCode).toBe(200));
-        });
-    }));
+  beforeEach(() => {
+    nock('https://jsonplaceholder.typicode.com')
+      .get('/albums/2')
+      .reply(200, mockResponse);
+  });
 
-  test('Should be a succesfull album query of an admin user', () =>
-    helper.createUserAndLogin(admin).then(logIn => {
-      agent
-        .post('/albums/2/')
-        .set('x-access-token', logIn.body.token)
-        .then(() => {
-          agent
-            .get('/users/1/albums')
-            .set('x-access-token', logIn.body.token)
-            .then(response => expect(response.statusCode).toBe(200));
-        });
-    }));
+  test('Should be a succesfull album query of a default user', () => {
+    const user = {
+      name: 'alan',
+      lastName: 'correa',
+      email: 'alan@wolox.co',
+      password: '$2b$10$Rz9Vh4/bNhclktWDcQ1zC.PF/Nlhx5YAQaQyeUMMyq5BDtb9fEGx6'
+    };
+    return User.create({ ...user, admin: true })
+      .then(() => token.createToken(user))
+      .then(tok =>
+        agent
+          .post('/albums/2')
+          .set('x-access-token', tok)
+          .then(() =>
+            agent
+              .get('/users/1/albums')
+              .set('x-access-token', tok)
+              .then(response => expect(response.statusCode).toBe(200))
+          )
+      );
+  });
+
+  test('Should be a succesfull album query of an admin user', () => {
+    const user = {
+      name: 'david',
+      lastName: 'montoya',
+      email: 'david@wolox.co',
+      password: '$2b$10$vn416Q0LFZ1uxwDnEYvnK.akT58G092KrFi5c2aaew6hFIZcgF4NO'
+    };
+    return User.create({ ...user, admin: true })
+      .then(() => token.createToken(user))
+      .then(tok =>
+        agent
+          .post('/albums/2')
+          .set('x-access-token', tok)
+          .then(() =>
+            agent
+              .get('/users/1/albums')
+              .set('x-access-token', tok)
+              .then(response => expect(response.statusCode).toBe(200))
+          )
+      );
+  });
 
   test('Sould fail due to authorization', () =>
-    helper.createUserAndLogin(!admin).then(logIn =>
+    helper.createUser(!admin).then(tok =>
       agent
         .get('/users/10/albums')
-        .set('x-access-token', logIn.body.token)
+        .set('x-access-token', tok)
         .then(response => expect(response.statusCode).toBe(401))
     ));
 });
--- test/app/testHelper.js
+++ test/app/testHelper.js
@@ -1,21 +1,12 @@
-const request = require('supertest');
-const app = require('../../app');
 const { User } = require('../../app/models');
+const token = require('../../app/helpers/token');
 
-exports.createUserAndLogin = type => {
+exports.createUser = type => {
   const user = {
     name: 'juan',
     lastName: 'gomez',
     email: 'juan@wolox.co',
-    password: '$2b$10$q0/nJGRvSyZz3i7fgvTY2OwMl4MPozMQI/62Bkz5F88tSl.3Y2W4u',
-    admin: type
+    password: '$2b$10$q0/nJGRvSyZz3i7fgvTY2OwMl4MPozMQI/62Bkz5F88tSl.3Y2W4u'
   };
-  return User.create(user).then(() =>
-    request(app)
-      .post('/users/sessions')
-      .send({
-        email: 'juan@wolox.co',
-        password: '12345678'
-      })
-  );
+  return User.create({ ...user, admin: type }).then(() => token.createToken(user));
 };
