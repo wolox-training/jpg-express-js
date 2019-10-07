136e8127afc965beef623bb5dd6156c120361946
jupabass89 <jupabass89@gmail.com>
2019-10-06 19:42:47 -0500

    run a new migration

---
+++ migrations/migrations/20191006203690-create-user.js
@@ -0,0 +1,11 @@
+const moment = require('moment');
+
+module.exports = {
+  up: (queryInterface, sequelize) =>
+    queryInterface.changeColumn('users', 'session', {
+      type: sequelize.INTEGER,
+      defaultValue: moment().unix(),
+      allowNull: true
+    }),
+  down: queryInterface => queryInterface.removeColumn('users', 'session')
+};
